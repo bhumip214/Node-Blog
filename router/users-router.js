4bd751221c17415ab3d316b09ef21c9444bc8aa9
bhumip214 <bhumip214@gmail.com>
2019-02-13 13:16:58 -0800

    Get user request

--- router/users-router.js
+++ router/users-router.js
@@ -2,8 +2,30 @@ const express = require("express");
 
 const Users = require("../data/helpers/userDb");
 
-const usersRouter = express.Router();
+const router = express.Router();
 
-usersRouter.use(express.json());
+router.use(express.json());
 
-module.exports = usersRouter;
+// GET users request
+router.get("/", async (req, res) => {
+  try {
+    const users = await Users.get(req.query);
+    res.status(200).json(users);
+  } catch (error) {
+    // log error to database
+    console.log(error);
+    res.status(500).json({
+      message: "Error retrieving the users"
+    });
+  }
+});
+
+//GET user by ID request
+
+// POST user request
+
+// DELETE user request
+
+// UPDATE user request
+
+module.exports = router;
