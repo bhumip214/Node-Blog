a180d9ba283b89e9112b5db46e04cabd8985e9d4
bhumip214 <bhumip214@gmail.com>
2019-02-13 14:22:10 -0800

    Get post by id request

--- router/posts-router.js
+++ router/posts-router.js
@@ -19,4 +19,24 @@ router.get("/", async (req, res) => {
   }
 });
 
+//GET post by ID request
+router.get("/:id", async (req, res) => {
+  try {
+    const post = await Posts.getById(req.params.id);
+
+    if (post) {
+      res.status(200).json(post);
+    } else {
+      res
+        .status(404)
+        .json({ message: "The post with the specified ID does not exist." });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "The post information could not be retrieved."
+    });
+  }
+});
+
 module.exports = router;
