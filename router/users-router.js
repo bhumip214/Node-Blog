8df1d671cd224629ea543a4536c54caedfacbb31
bhumip214 <bhumip214@gmail.com>
2019-02-13 13:18:58 -0800

    Get user by Id request

--- router/users-router.js
+++ router/users-router.js
@@ -21,6 +21,22 @@ router.get("/", async (req, res) => {
 });
 
 //GET user by ID request
+router.get("/:id", async (req, res) => {
+  try {
+    const user = await Users.getById(req.params.id);
+
+    if (user) {
+      res.status(200).json(user);
+    } else {
+      res.status(404).json({ message: "User not found" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Error retrieving the user"
+    });
+  }
+});
 
 // POST user request
 
